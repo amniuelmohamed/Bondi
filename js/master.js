f469514c4fef8259b86c80fdb5f743430e18516b
Amniuel Merabet Mohamed <amniuel.mohamed@gmail.com>
2023-09-11 23:15:56 +0200

    Adding the about us section and enhancing the animation

--- js/master.js
+++ js/master.js
@@ -1,15 +1,12 @@
 // Animate the feature section
-let toAnimate = Array.from(document.querySelectorAll(".animate"));
-window.addEventListener("scroll", () => {
-    toAnimate.forEach((el, idx) => {
-        if (
-            window.innerHeight + window.scrollY >= el.offsetTop &&
-            !el.classList.contains("showed")
-        ) {
-            // Add showed class to begin animation
-            el.classList.add("showed");
-            // Remove this element from the array because we dont need it anymore
-            toAnimate.splice(idx, 1);
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((el) => {
+        if (el.isIntersecting) {
+            el.target.classList.add("showed");
         }
     });
 });
+
+let toAnimate = document.querySelectorAll(".hidden");
+
+toAnimate.forEach((el) => observer.observe(el));
